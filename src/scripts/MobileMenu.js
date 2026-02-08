e773f4cb7f9d6e5b798a0d706993650f723c01f3
Dima K <gapfild@gmail.com>
2026-02-08 23:49:22 +0200

    mobile menu with second level

--- src/scripts/MobileMenu.js
+++ src/scripts/MobileMenu.js
@@ -1,114 +1,122 @@
+import { mobileMenuStyles } from './MobileMenu.styles.js';
 class MobileMenu extends HTMLElement {
   constructor() {
     super();
-    this.attachShadow({ mode: "open" });
-    this.isOpen = false;
+    this.attachShadow({ mode: "open" })
+    this.shadowRoot.adoptedStyleSheets = [mobileMenuStyles];
+    this.isOpen = false
+    this.isSecondLevelMenuOpen = false
+    this.activeSubMenuId = null;
   }
 
   connectedCallback() {
-    this.render();
-    const btn = document.querySelector(".mobile-menu-btn");
-    if (btn) {
-      btn.addEventListener("click", () => this.toggleMenu());
-    }
+    this.render()
+    this.cacheElements()
+
+    const externalBtn = document.querySelector(".mobile-menu-btn")
+    externalBtn?.addEventListener("click", () => this.toggleMenu())
+
+    this.shadowRoot.addEventListener('click', (e) => {
+      const target = e.target
+
+      const childMenuTrigger = target.closest('[data-child-links-list-Id]')
+      const isBackBtn = Boolean(target.closest('.drawer-header-btn')) && this.isSecondLevelMenuOpen
+      const isCloseBtn = Boolean(target.closest('.close-btn'))
+      const isOverlay = Boolean(target.closest('.overlay'))
+
+      switch (true) {
+        case !!childMenuTrigger:
+          e.preventDefault()
+          this.activeSubMenuId = childMenuTrigger.dataset.childLinksListId
+          this.toggleSecondMenuOpen(true)
+          this.updateStyles()
+          break
+
+        case isBackBtn:
+          this.isSecondLevelMenuOpen = !this.isSecondLevelMenuOpen
+          this.toggleSecondMenuOpen(this.isSecondLevelMenuOpen)
+          this.updateStyles()
+          break
+
+        case isCloseBtn:
+        case isOverlay:
+          this.toggleMenu()
+          break
+      }
+
+    })
+  }
+
+  disconnectedCallback() {
+    const externalBtn = document.querySelector(".mobile-menu-btn");
+    externalBtn?.removeEventListener("click", this._boundToggle);
+  }
+
+  cacheElements() {
+    this._drawer = this.shadowRoot.querySelector(".drawer")
+    this._overlay = this.shadowRoot.querySelector(".overlay")
+    this._headerBtn = this.shadowRoot.querySelector(".drawer-header-btn")
+    this._secondLevelMenus = this.shadowRoot.querySelector("slot[name='second-level-menus']")
+    this._drawerContentHolder = this.shadowRoot.querySelector(".drawer-content")
+    this._defaultSlot = this.shadowRoot.querySelector(".main-slot slot:not([name])")
+    this._secondarySlot = this.shadowRoot.querySelector("slot[name='secondary']")
   }
 
   toggleBodyScroll() {
-    if (this.isOpen) {
-      document.body.style.overflow = "hidden";
-      document.body.style.paddingRight = "var(--scrollbar-width, 0px)";
-    } else {
-      document.body.style.overflow = "";
-      document.body.style.paddingRight = "";
-    }
+    document.body.style.overflow = this.isOpen ? "hidden" : ""
+    document.body.style.paddingRight = this.isOpen ? "var(--scrollbar-width, 0px)" : ""
   }
 
   toggleMenu() {
-    this.isOpen = !this.isOpen;
-    this.updateStyles();
-    this.toggleBodyScroll();
+    this.isOpen = !this.isOpen
+    if (!this.isOpen) {
+      this.toggleSecondMenuOpen(false)
+    }
+    this.updateStyles()
+    this.toggleBodyScroll()
+  }
+
+  toggleSecondMenuOpen(state) {
+    this.isSecondLevelMenuOpen = state
+    if (!state) {
+      this.activeSubMenuId = null
+    }
+    this._drawerContentHolder?.classList.toggle("second-level-menu-open", state)
   }
 
   updateStyles() {
-    const drawer = this.shadowRoot.querySelector(".drawer");
-    const overlay = this.shadowRoot.querySelector(".overlay");
+    const isSubMenuOpen = !!this.activeSubMenuId;
+    this._drawer.classList.toggle("open", this.isOpen)
+    this._overlay.classList.toggle("active", this.isOpen)
+    this._headerBtn.classList.toggle("is-back-state", this.isSecondLevelMenuOpen)
 
-    if (this.isOpen) {
-      drawer.classList.add("open");
-      overlay.classList.add("active");
-    } else {
-      drawer.classList.remove("open");
-      overlay.classList.remove("active");
+    const slottedWrapper = this._secondLevelMenus.assignedElements()[0]
+
+    if (slottedWrapper) {
+      const subMenus = slottedWrapper.querySelectorAll(".secondary-menu-item")
+
+      subMenus.forEach((menu) => {
+        const parentId = menu.dataset.parentLinkId
+        const isMatch = parentId === this.activeSubMenuId
+        menu.classList.toggle("is-active", isMatch)
+      })
     }
+
+    const allPrimaryElements = [
+        ...this._defaultSlot.assignedElements(),
+        ...this._secondarySlot.assignedElements()
+    ];
+
+    allPrimaryElements.forEach(el => {
+        if (el.classList.contains("drawer-navigation-links") ||
+            el.classList.contains("drawer-secondary-links")) {
+            el.classList.toggle("is-hidden", isSubMenuOpen);
+        }
+    });
   }
 
   render() {
     this.shadowRoot.innerHTML = `
-                <style>
-                    .drawer {
-                      position: fixed;
-                      top: 0;
-                      left: -100%;
-                      width: 100%;
-                      height: 100dvh;
-                      max-width: 43rem;
-                      background: var(--white);
-                      transition: 0.3s;
-                      z-index: 120;
-                      box-shadow: -0.2rem 0 .5rem rgba(var(--black),0.5);
-                    }
-                    .drawer-content-wrapper {
-                      background: var(--background-color);
-                      color: var(--primary-color);
-                      height: 100%;
-                      padding-inline: 2.2rem;
-                      display: flex;
-                      flex-direction: column;
-                    }
-                    .drawer.open {
-                      left: 0;
-                    }
-                    .drawer-announcement-bar {
-                      display: flex;
-                      justify-content: center;
-                      padding-block: 2.7rem;
-                    }
-                    .drawer-header {
-                      display: flex;
-                      justify-content: space-between;
-                      align-items: center;
-                    }
-                    .drawer-content {
-                      flex: 1;
-                      display: flex;
-                      flex-direction: column;
-                      justify-content: space-between;
-                      max-height: calc(100% - 20rem);
-                    }
-                    .close-btn {
-                      background: transparent;
-                      border: none;
-                      cursor: pointer;
-                      padding: 0;
-                    }
-                    .overlay {
-                        position: fixed;
-                        top: 0;
-                        left: 0;
-                        width: 100%;
-                        height: 100%;
-                        background: rgba(0,0,0, .5);
-                        visibility: hidden;
-                        opacity: 0;
-                        transition: 0.3s;
-                        z-index: 110;
-                    }
-                    .overlay.active {
-                      visibility: visible;
-                      opacity: 1;
-                    }
-                </style>
-
                 <div class="overlay"></div>
 
                 <div class="drawer">
@@ -119,16 +127,30 @@ class MobileMenu extends HTMLElement {
                       </div>
                     </div>
                     <div class="drawer-header">
-                      <svg width="68px" height="22px" viewBox="0 0 68 22" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
-                          <title>Vilo</title>
-                          <g stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
-                              <g transform="translate(-120, -119)" fill="currentColor">
-                                  <g transform="translate(80, 95)">
-                                      <path d="M64.7897289,45.647692 L64.7897289,26.5938876 C64.7897289,25.280409 65.8545136,24.2156243 67.1679921,24.2156243 L68.6073903,24.2156243 L68.6073903,24.2156243 L68.6073903,43.2694288 C68.6073903,44.5829073 67.5426055,45.647692 66.229127,45.647692 L64.7897289,45.647692 L64.7897289,45.647692 Z M48.9679478,45.647692 L40,24.2156243 L42.5683517,24.2156243 C43.5471696,24.2156243 44.4260232,24.8153271 44.7829028,25.7267664 L50.6937385,40.8225202 L50.6937385,40.8225202 L56.5751707,25.730333 C56.9310901,24.8170176 57.810899,24.2156243 58.7911151,24.2156243 L61.2642061,24.2156243 L61.2642061,24.2156243 L52.9072753,44.1874519 C52.5371719,45.0719444 51.6721392,45.647692 50.713336,45.647692 L48.9679478,45.647692 L48.9679478,45.647692 Z M85.2637055,45.647692 L79.3936475,45.647692 C75.8573592,45.647692 72.9906311,42.780964 72.9906311,39.2446757 L72.9906311,24.2156243 L72.9906311,24.2156243 L74.7959159,24.2156243 C75.9073208,24.2156243 76.8082925,25.116596 76.8082925,26.2280009 L76.8082925,38.7006179 C76.8082925,40.7213541 78.4464228,42.3594844 80.467159,42.3594844 L85.2637055,42.3594844 L85.2637055,42.3594844 L85.2637055,45.647692 Z M95.9565376,24 C96.0677716,24 96.1694186,24.00398 96.2614786,24.0119399 C96.3624098,24.0172663 96.4375999,24.1053788 96.4296295,24.2061007 C96.427402,24.2342498 96.4186888,24.2615011 96.404172,24.2857209 C95.1806406,26.2707804 94.4456676,27.6247605 94.1986373,28.3472924 L94.1771421,28.4135756 C93.6384774,28.7392611 93.1370288,29.1553087 92.6727965,29.6617182 C91.3719637,31.0807364 90.7215473,32.8373831 90.7215473,34.9316582 C90.7215473,37.0259333 91.3719637,38.7825799 92.6727965,40.2015981 C93.9736293,41.6206163 95.5666781,42.3301254 97.451943,42.3301254 C99.3372079,42.3301254 100.930257,41.6206163 102.231089,40.2015981 C103.531922,38.7825799 104.182339,37.0259333 104.182339,34.9316582 C104.182339,32.8373831 103.531922,31.0807364 102.231089,29.6617182 C100.930257,28.2427001 99.3372079,27.533191 97.451943,27.533191 C96.990693,27.533191 96.5469347,27.5756613 96.1206682,27.660602 C96.7705951,26.5384299 98.7667982,25.1706274 99.1372199,24.9613765 C99.5076416,24.7521256 99.8553169,24.5398373 100.51124,24.4800949 C100.756538,24.4673448 101.026596,24.5053348 101.321413,24.594065 C102.640317,25.1345725 103.838715,25.9719906 104.917592,27.1074855 C106.972531,29.270265 108,31.8783225 108,34.9316582 C108,37.9849939 106.972531,40.5930514 104.917592,42.7558308 C102.862653,44.9186103 100.374104,46 97.451943,46 C94.5297824,46 92.0412328,44.9186103 89.986294,42.7558308 C87.9313553,40.5930514 86.9038859,37.9849939 86.9038859,34.9316582 C86.9038859,31.8783225 87.9313553,29.270265 89.986294,27.1074855 C91.5373922,25.4749876 93.3355382,24.4586064 95.3807321,24.0583421 C95.7050978,24.0168239 95.8819868,24 95.9565376,24 Z" fill-rule="nonzero"></path>
+                      <div class="drawer-header-btn">
+                        <div class="icon-back" aria-label="Back to main menu">
+                          <svg width="10px" height="16px" viewBox="0 0 10 16" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
+                              <title>Back</title>
+                              <g stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
+                                  <g transform="translate(-22, -149)" fill="currentColor" fill-rule="nonzero">
+                                      <path d="M30.6183185,149.32494 C30.9883505,148.954908 31.5248804,148.891497 31.8166916,149.183308 C32.1085028,149.47512 32.0450923,150.01165 31.6750603,150.381681 L25.067,156.989 L31.6750603,163.618319 C32.0450923,163.98835 32.1085028,164.52488 31.8166916,164.816692 C31.5248804,165.108503 30.9883505,165.045092 30.6183185,164.67506 L22.9330038,157 Z"></path>
                                   </g>
                               </g>
-                          </g>
-                      </svg>
+                          </svg>
+                        </div>
+                        <div class="icon-logo" >
+                          <svg width="68px" height="22px" viewBox="0 0 68 22" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" >
+                            <title>Vilo</title>
+                            <g stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
+                                <g transform="translate(-120, -119)" fill="currentColor">
+                                    <g transform="translate(80, 95)">
+                                        <path d="M64.7897289,45.647692 L64.7897289,26.5938876 C64.7897289,25.280409 65.8545136,24.2156243 67.1679921,24.2156243 L68.6073903,24.2156243 L68.6073903,24.2156243 L68.6073903,43.2694288 C68.6073903,44.5829073 67.5426055,45.647692 66.229127,45.647692 L64.7897289,45.647692 L64.7897289,45.647692 Z M48.9679478,45.647692 L40,24.2156243 L42.5683517,24.2156243 C43.5471696,24.2156243 44.4260232,24.8153271 44.7829028,25.7267664 L50.6937385,40.8225202 L50.6937385,40.8225202 L56.5751707,25.730333 C56.9310901,24.8170176 57.810899,24.2156243 58.7911151,24.2156243 L61.2642061,24.2156243 L61.2642061,24.2156243 L52.9072753,44.1874519 C52.5371719,45.0719444 51.6721392,45.647692 50.713336,45.647692 L48.9679478,45.647692 L48.9679478,45.647692 Z M85.2637055,45.647692 L79.3936475,45.647692 C75.8573592,45.647692 72.9906311,42.780964 72.9906311,39.2446757 L72.9906311,24.2156243 L72.9906311,24.2156243 L74.7959159,24.2156243 C75.9073208,24.2156243 76.8082925,25.116596 76.8082925,26.2280009 L76.8082925,38.7006179 C76.8082925,40.7213541 78.4464228,42.3594844 80.467159,42.3594844 L85.2637055,42.3594844 L85.2637055,42.3594844 L85.2637055,45.647692 Z M95.9565376,24 C96.0677716,24 96.1694186,24.00398 96.2614786,24.0119399 C96.3624098,24.0172663 96.4375999,24.1053788 96.4296295,24.2061007 C96.427402,24.2342498 96.4186888,24.2615011 96.404172,24.2857209 C95.1806406,26.2707804 94.4456676,27.6247605 94.1986373,28.3472924 L94.1771421,28.4135756 C93.6384774,28.7392611 93.1370288,29.1553087 92.6727965,29.6617182 C91.3719637,31.0807364 90.7215473,32.8373831 90.7215473,34.9316582 C90.7215473,37.0259333 91.3719637,38.7825799 92.6727965,40.2015981 C93.9736293,41.6206163 95.5666781,42.3301254 97.451943,42.3301254 C99.3372079,42.3301254 100.930257,41.6206163 102.231089,40.2015981 C103.531922,38.7825799 104.182339,37.0259333 104.182339,34.9316582 C104.182339,32.8373831 103.531922,31.0807364 102.231089,29.6617182 C100.930257,28.2427001 99.3372079,27.533191 97.451943,27.533191 C96.990693,27.533191 96.5469347,27.5756613 96.1206682,27.660602 C96.7705951,26.5384299 98.7667982,25.1706274 99.1372199,24.9613765 C99.5076416,24.7521256 99.8553169,24.5398373 100.51124,24.4800949 C100.756538,24.4673448 101.026596,24.5053348 101.321413,24.594065 C102.640317,25.1345725 103.838715,25.9719906 104.917592,27.1074855 C106.972531,29.270265 108,31.8783225 108,34.9316582 C108,37.9849939 106.972531,40.5930514 104.917592,42.7558308 C102.862653,44.9186103 100.374104,46 97.451943,46 C94.5297824,46 92.0412328,44.9186103 89.986294,42.7558308 C87.9313553,40.5930514 86.9038859,37.9849939 86.9038859,34.9316582 C86.9038859,31.8783225 87.9313553,29.270265 89.986294,27.1074855 C91.5373922,25.4749876 93.3355382,24.4586064 95.3807321,24.0583421 C95.7050978,24.0168239 95.8819868,24 95.9565376,24 Z" fill-rule="nonzero"></path>
+                                    </g>
+                                </g>
+                            </g>
+                          </svg>
+                        </div>
+                      </div>
                       <button class="close-btn" role="button" aria-label="Close menu">
                         <svg width="42px" height="42px" viewBox="0 0 42 42" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
                           <title>Close menu</title>
@@ -151,12 +173,13 @@ class MobileMenu extends HTMLElement {
                       <div class="secondary-slot">
                         <slot name="secondary"></slot>
                       </div>
+                      <div class="secondary-menus-slot">
+                        <slot name="second-level-menus"></slot>
+                      <div>
                     </div>
                   </div>
                 </div>
                 `;
-    this.shadowRoot.querySelector('.close-btn').onclick = () => this.toggleMenu();
-    this.shadowRoot.querySelector(".overlay").onclick = () => this.toggleMenu();
   }
 }
 
